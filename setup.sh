1ddd634a3c6608bcc6dc00c60f13fe9db54bea88
Birmjune Kim <83191737+Birmjune@users.noreply.github.com>
2025-06-18 21:02:54 +0900

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Note that when we execute shell script file in bash,
+# conda activate env && pip install ... not works properly.
+# You should use conda run -n env <cmd>
+# see the below example.
+#
+# If you want to create another environment, then set EVAL_ENV
+# But we do not recommend it, because creating new environment
+# and installing packages from scratch takes a lot of time,
+# that can impact time limit of evaluation.
+# 
+# export EVAL_ENV=testenv
+# conda create -n $EVAL_ENV python=3.10 -y
+# conda run -n $EVAL_ENV pip install -r /workspace/evaluate/artifacts/requirements.txt
+# export EVAL_ENV=project
+# conda create -n $EVAL_ENV python=3.11 -y
+export EVAL_ENV=project
+export HF_TOKEN= (your token)
+#conda run -n $EVAL_ENV pip install -r ./artifacts/requirements.txt
